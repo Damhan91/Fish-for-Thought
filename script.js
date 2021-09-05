e30ea265e3a7be10592c0d8a5e0b100859679c20
Damhan91 <damhanp@gmail.com>
2021-09-05 14:14:05 +0000

    changed from class to id for buttons elements

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener("click", displayDate);
 
 function displayDate() {
-  document.getElementById("imagehere").innerHTML = Date();}
+  document.getElementById("imagehere").innerHTML = "1 Betta Fish";}
