079a7d9820e8263c1e23684ece97520f5f432f34
Damhan91 <damhanp@gmail.com>
2021-09-05 15:10:53 +0000

    added functions to the 4 buttons, shwoing text when clicked

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-document.addEventListener("click", displayDate);
+document.getElementById("button1").addEventListener("click", button);
 
-function displayDate() {
-  document.getElementById("imagehere").innerHTML = "1 Betta Fish";}
+
+function button() {    
+  document.getElementById("button1").onclick=function() {
+  document.getElementById("imagehere").innerHTML= "1 Betta Fish";
+;}}
+
+
+document.getElementById("button1").addEventListener("click", button1);
+
+function button1() {    
+  document.getElementById("button2").onclick=function() {
+  document.getElementById("imagehere").innerHTML= "1 Honey Gourami, 2 Panda corydoras";
+;}}
+
+document.getElementById("button1").addEventListener("click", button2);
+
+function button2() {    
+  document.getElementById("button3").onclick=function() {
+  document.getElementById("imagehere").innerHTML= "1 Angelfish, 5 Neon Tetras, 5 Brone corydoras";
+;}}
+
+document.getElementById("button1").addEventListener("click", button3);
+
+function button3() {    
+  document.getElementById("button4").onclick=function() {
+  document.getElementById("imagehere").innerHTML= "2 Angelfish, 8 Rummy Tetras, 6 Juli corydoras, 1 Hillstream loach";
+;}}
