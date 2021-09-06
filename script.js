dd610ea405a743c32efd2d929fb7f49622329e72
Damhan91 <damhanp@gmail.com>
2021-09-06 19:05:26 +0000

    edit the event listner for each button

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-document.getElementById("button1").addEventListener("click", button);
+document.addEventListener("click", button1);
 
-
-function button() {    
+function button1() {    
   document.getElementById("button1").onclick=function() {
   document.getElementById("imagehere").innerHTML= "1 Betta Fish";
 ;}}
 
 
-document.getElementById("button1").addEventListener("click", button1);
+document.addEventListener("click", button2);
 
-function button1() {    
+function button2() {    
   document.getElementById("button2").onclick=function() {
   document.getElementById("imagehere").innerHTML= "1 Honey Gourami, 2 Panda corydoras";
 ;}}
 
-document.getElementById("button1").addEventListener("click", button2);
+document.addEventListener("click", button3);
 
-function button2() {    
+function button3() {    
   document.getElementById("button3").onclick=function() {
   document.getElementById("imagehere").innerHTML= "1 Angelfish, 5 Neon Tetras, 5 Brone corydoras";
 ;}}
 
-document.getElementById("button1").addEventListener("click", button3);
+document.addEventListener("click", button4);
 
-function button3() {    
+function button4() {    
   document.getElementById("button4").onclick=function() {
   document.getElementById("imagehere").innerHTML= "2 Angelfish, 8 Rummy Tetras, 6 Juli corydoras, 1 Hillstream loach";
 ;}}
